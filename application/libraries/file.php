2c48f365d7ba343a7889d502ca19b235084f8d66
USER <user@USERde-iMac.local>
2017-03-20 20:29:38 +0800

    change to ci

--- application/libraries/file.php
+++ application/libraries/file.php
@@ -17,7 +17,7 @@ class File{
 					if( in_array($file, $this->except_dir) ) continue 1;
 					$real_path = str_replace(DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, $path.DIRECTORY_SEPARATOR.$file);
 					if(is_file($real_path)){
-						$result[$real_path] = iconv("utf-8","gb2312",$file);
+						$result[$real_path] = iconv('GB2312','UTF-8',$file);
 					}
 					if (is_dir($real_path)){
 						$queue[] = $real_path;
