66a742a6cc0f5c4ccc7503413d053cde331eecc4
USER <user@USERde-iMac.local>
2017-03-20 19:36:16 +0800

    update struct 22

--- assets/js/contentCtrl.js
+++ assets/js/contentCtrl.js
@@ -1,9 +1,10 @@
 mmAdmin.controller('contentCtrl', function($scope, contentServ){
-	$scope.taskList = [0,1,2,3,4,5];
+	$scope.taskList = [];
 
 	//$scope.$on("$ionicView.beforeEnter", function(){
 		contentServ.getTaskFile().then(function(data){
 			console.log(data);
+			$scope.taskList = data;
 		})
 	//});
 
