03a7902eda38e5a75264d43b9df373b573d8caf2
USER <user@USERde-iMac.local>
2017-04-01 12:11:30 +0800

    a

--- application/views/upload_detail_view.php
+++ application/views/upload_detail_view.php
@@ -18,6 +18,7 @@
 				<div class="item upload">
 					<div class="upload-detail-file ">
 						<div class="file-name"><?=$file['file_name']?></div>
+						<div class="file-dir"><?=$v['file_dir']?></div>
 						<div class="info-item">
 							<select name="doc_cate_id">
 								<option value="1" selected="selected">地理</option>
@@ -27,7 +28,7 @@
 							</select>
 						</div>
 						<div class="upload-detail-act">
-							<button type="submit" class="">Exec</button>
+							<button type="submit" class="exec">Exec</button>
 						</div>
 					</div>
 				</div>
