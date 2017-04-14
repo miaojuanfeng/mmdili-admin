8936a3592354691ed856f7b9c46b0a695e539e28
miaojuanfeng <miaojuanfeng@qq.com>
2017-04-14 18:24:00 +0800

    ag

---
+++ application/logs/log-2017-04-14.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-04-14 14:46:53 --> 404 Page Not Found: /index
+ERROR - 2017-04-14 18:23:27 --> 404 Page Not Found: /index
+ERROR - 2017-04-14 18:23:27 --> 404 Page Not Found: Faviconico/index
