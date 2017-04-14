abee41d22463e43833ea7ee52ac677188fe185ba
miaojuanfeng <miaojuanfeng@qq.com>
2017-04-14 18:55:12 +0800

    a

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://47.92.3.88/admin';
+$config['base_url'] = 'http://admin.mmdili.com';
 
 /*
 |--------------------------------------------------------------------------
