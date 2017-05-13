5cfb2d10d325873ac1e2803aee3aa92db4ba754b
miaojuanfeng <miaojuanfeng@qq.com>
2017-05-13 17:31:31 +0800

    add doc management

--- application/models/Doc_model.php
+++ application/models/Doc_model.php
@@ -4,7 +4,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-class upload_model extends CI_Model{
+class doc_model extends CI_Model{
     //put your code here
     
     function __construct()
