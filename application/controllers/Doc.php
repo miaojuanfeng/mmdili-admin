81cdfb9b9ad8340183abce1e0efad618754f889a
miaojuanfeng <miaojuanfeng@qq.com>
2017-05-24 01:04:10 +0800

    a

--- application/controllers/Doc.php
+++ application/controllers/Doc.php
@@ -295,7 +295,7 @@ class Doc extends CI_Controller {
 					$cmd .= ' --embed-css 0';
 					$cmd .= ' --embed-font 0';
 					$cmd .= ' --bg-format "jpg"';
-					$cmd .= ' --dest-dir "'.$view_path.'"';
+					$cmd .= ' --dest-dir "C:\\MJF\\web\\doc\\view\\1495349360\\"';
 					$cmd .= ' --page-filename "'.$doc_url.'-%03d.page"';
 					$cmd .= ' --css-filename "'.$doc_url.'.css"';
 					$cmd .= ' --embed-javascript 0';
