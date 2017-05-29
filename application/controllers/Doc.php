9207d46a1d4d525227d1203c10b37b2c21109df8
miaojuanfeng <miaojuanfeng@qq.com>
2017-05-29 15:01:06 +0800

    a

--- application/controllers/Doc.php
+++ application/controllers/Doc.php
@@ -329,7 +329,8 @@ class Doc extends CI_Controller {
 						preg_match_all('/<img.+src=\"?(.+\.(jpg|gif|bmp|bnp|png))\"?.+>/i', $file_content, $imgArr);
 						$file_content = str_replace($imgArr[1][0], 'http://view.mmdili.com/'.$user_url.'/'.$doc_url.'/'.$imgArr[1][0], $file_content);
 						//
-						preg_match_all('/<\s*img\s+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i', $file_content, $imgArr); 
+						// preg_match_all('/<\s*img\s+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i', $file_content, $imgArr); 
+ 						preg_match_all('/<\s*img\s+[^>]*?class\s*=\s*(\'|\")(.*?)\\1+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i', $file_content, $imgArr); 
  						var_dump($imgArr);die();
 						//
 						preg_match_all('/<a .*?href="(.*?)".*?>/is', $file_content, $imgArr);
