aa6044cafefd14bb67f7d2902fb69afd86d2d98e
USER <user@USERde-iMac.local>
2017-05-31 18:55:02 +0800

    a

--- application/controllers/Doc.php
+++ application/controllers/Doc.php
@@ -658,7 +658,7 @@ class Doc extends CI_Controller {
 					}
 
 					if( $this->doc_model->update($doc_id, $doc_cate_id, $doc_user_id, $doc_dl_forbidden, $update_doc_content, $doc_content, $update_doc_html) ){
-						redirect(base_url('doc/detail/'.$doc_id));
+						// redirect(base_url('doc/detail/'.$doc_id));
 					}
 				}
 			}
