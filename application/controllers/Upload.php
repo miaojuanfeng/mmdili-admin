a04edfac8034b7a91c39579df5684a8948a2c682
USER <user@USERde-iMac.local>
2017-07-20 19:11:10 +0800

    asd

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -361,6 +361,39 @@ pdf2swf_run:
 		header('Location:'.base_url('upload'));
 	}
 
+	private function clearn_file($path, $file_type){
+	    //判断要清除的文件类型是否合格
+	    if(!preg_match('/^[a-zA-Z]{2,}$/',$file_type)){
+	        return false;
+	    }
+	    //当前路径是否为文件夹或可读的文件
+	    if(!is_dir($path)||!is_readable($path)){
+	        return false;
+	    }
+	    //遍历当前目录下所有文件
+	    $all_files=scandir($path);
+	    foreach($all_files as $filename){
+	        //跳过当前目录和上一级目录
+	        if(in_array($filename,array(".", ".."))){
+	            continue;
+	        }
+	        //进入到$filename文件夹下
+	        $full_name=$path.'/'.$filename;
+	        //判断当前路径是否是一个文件夹，是则递归调用函数
+	        //否则判断文件类型，匹配则删除
+	        if(is_dir($full_name)){
+	            clearn_file($full_name,$file_type);
+	        }else{
+	            preg_match("/(.*)\.$file_type/",$filename,$match);
+	            if(!empty($match[0][0]) || !empty($match[0])){
+	                //echo $full_name;
+	                //echo '<br>';
+	                unlink($full_name);
+	            }
+	        }
+	    }
+	}
+
 	public function exec_html()
 	{
 		$file_index       = $this->input->post('file_index');
