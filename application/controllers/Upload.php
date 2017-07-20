7e68807b4353b8ae5ba6f2e0dc73cec0e2141779
USER <user@USERde-iMac.local>
2017-07-20 17:38:50 +0800

    asd

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -360,4 +360,297 @@ pdf2swf_run:
 		$this->upload_model->insert_doc($time, iconv('GB2312', 'UTF-8', $file['filename']), $doc_content, $doc_user_id, $doc_ext, $doc_cate_id, $page_width, $page_height, $page_num, intval(!empty($poly2bitmap)), $doc_dl_forbidden);
 		header('Location:'.base_url('upload'));
 	}
+
+	public function exec_html()
+	{
+		$file_index       = $this->input->post('file_index');
+		$doc_cate_id      = $this->input->post('doc_cate_id');
+		$doc_dl_forbidden = $this->input->post('doc_dl_forbidden');
+		$doc_user_id      = $this->input->post('doc_user_id');
+		$doc_content      = "";
+		if( ($file_index != 0 && empty($file_index)) || empty($doc_cate_id) ){
+			echo 'paremeters wrong!';
+			return;
+		}
+		if(!count(self::$exists_files) || !isset(self::$exists_files[$file_index])){
+			echo 'index not exists!';
+			return;
+		}
+		$file_path = iconv('UTF-8', 'GB2312', self::$exists_files[$file_index]['file_dir']);
+		if( !file_exists($file_path) ){
+			echo 'file not exists!';
+			return;
+		}
+		$file = $this->mb_pathinfo($file_path);
+		if( !$this->upload_model->check_doc_exists(iconv('GB2312', 'UTF-8', $file['filename'])) ){
+			echo 'database record already exists!';
+			return;
+		}
+		$time = time();
+		$online_path = self::$online_path.$this->user_url[$doc_user_id].'\\'.strtotime(date('Y', $time).'-01-01 00:00:00').'\\';
+		if( file_exists($online_path.$file['basename']) ){
+			echo 'file already exists!';
+			return;
+		}
+		rename($file_path, self::$convert_path.$file['basename']);
+		if( $file['extension'] == 'doc' || $file['extension'] == 'docx' || $file['extension'] == 'txt' ){
+			try{
+				$word = null;
+	    			$word = new COM("Word.Application") or die ("Could not initialise Word Object.");   
+				$retry = 60;
+				while( !$word && (--$retry) ){
+					sleep(1);
+				}
+				if( $retry <= 0 ){
+					echo "word application not ready!";
+					rename(self::$convert_path.$file['basename'], $file_path);
+					return;
+				}
+	   			$word->Visible = 0;   
+	    		$word->DisplayAlerts = 0; 
+				$word->Documents->Open(self::$convert_path.$file['basename']);
+				$doc_content = $word->ActiveDocument->content->Text;
+				$word->ActiveDocument->ExportAsFixedFormat(self::$convert_path.$file['filename'].'.pdf', 17, false, 0, 0, 0, 0, 7, true, true, 2, true, true, false);
+				$word->Quit(false);  
+				unset($word);
+			}catch(Exception $e){
+				if( $word ){
+	    			$word->Quit(false);  
+	    			unset($word);
+				}
+				rename(self::$convert_path.$file['basename'], $file_path);
+				echo $e->getMessage();
+				return;
+			}
+		}
+		if( $file['extension'] == 'ppt' || $file['extension'] == 'pptx' ){
+			try{
+				$ppt = null;
+	    			$ppt = new COM("powerpoint.Application") or die ("Could not initialise PowerPoint Object.");   
+				$retry = 60;
+				while( !$ppt && (--$retry) ){
+					sleep(1);
+				}
+				if( $retry <= 0 ){
+					echo "ppt application not ready!";
+					rename(self::$convert_path.$file['basename'], $file_path);
+					return;
+				}
+	   			$ppt->Visible = true; //Hiding the application window is not allowed.   
+	    		$ppt->DisplayAlerts = 0; 
+				$ppt->Presentations->Open(self::$convert_path.$file['basename']);
+foreach($ppt->ActivePresentation->Slides as $k1 => $v1){
+	foreach($v1->Shapes as $k2 => $v2 ){
+		if( $v2->HasTextFrame && $v2->TextFrame->HasText ){
+			$doc_content .= $v2->TextFrame->TextRange->Text." ";
+		}
+		if( $v2->HasTable ){
+			foreach($v2->Table->Rows as $k3 => $v3){
+				foreach($v2->Table->Columns as $k4 => $v4){
+					$doc_content .= $v2->Table->Cell($k3+1, $k4+1)->Shape->TextFrame->TextRange->Text." ";
+				}
+			}
+		}
+	}
+}
+				$ppt->ActivePresentation->SaveAs(self::$convert_path.$file['filename'].'.pdf', 32);
+				$ppt->Quit();  
+				unset($ppt);
+			}catch(Exception $e){
+				if( $ppt ){
+	    			$ppt->Quit();  
+	    			unset($ppt);
+				}
+				rename(self::$convert_path.$file['basename'], $file_path);
+				echo $e->getMessage();
+				return;
+			}
+		}
+		if( $file['extension'] == 'xls' || $file['extension'] == 'xlsx' ){
+			try{
+				$excel = null;
+	    			$excel = new COM("excel.Application") or die ("Could not initialise Excel Object.");   
+				$retry = 60;
+				while( !$excel && (--$retry) ){
+					sleep(1);
+				}
+				if( $retry <= 0 ){
+					echo "excel application not ready!";
+					rename(self::$convert_path.$file['basename'], $file_path);
+					return;
+				}
+	   			$excel->Visible = 1;   
+	    		$excel->DisplayAlerts = 0; 
+				$excel->Workbooks->Open(self::$convert_path.$file['basename']);
+				$excel->Workbooks[1]->ExportAsFixedFormat(0, self::$convert_path.$file['filename'].'.pdf');
+				$excel->Workbooks[1]->Close(false);
+				$excel->Quit();  
+				unset($excel);
+			}catch(Exception $e){
+				if( $excel ){
+				$excel->Workbooks[1]->Close(false);
+	    			$excel->Quit();  
+	    			unset($excel);
+				}
+				rename(self::$convert_path.$file['basename'], $file_path);
+				echo $e->getMessage();
+				return;
+			}
+		}
+
+		$view_path = self::$view_path.$doc_url;
+
+		$page_num = 0;
+		$page_width = 0;
+		$page_height = 0;
+		$pdf_info = array();
+		$exec = "C:\MJF\SWFTools\pdf2swf.exe \"".self::$convert_path.$doc_url.".pdf\" -I";
+		exec($exec, $pdf_info);
+		if( count($pdf_info) ){
+			$page_num = count($pdf_info);
+			$page_width_height_string = explode(' ', $pdf_info[0]);
+			$page_width  = intval(explode('=', $page_width_height_string[1])[1]);
+			$page_height = intval(explode('=', $page_width_height_string[2])[1]);
+		}
+		if( !$page_width || !$page_height || !$page_num ){
+			echo "get pdf info failed.";
+			return;
+		}
+
+		$cmd  = 'C:\MJF\pdf2htmlEX\pdf2htmlEX.exe';
+		// $cmd .= ' --zoom 1.613';
+		$cmd .= ' --fit-width 960';
+		$cmd .= ' --space-as-offset 1';
+		$cmd .= ' --split-pages 1';
+		$cmd .= ' --embed-image 0';
+		$cmd .= ' --embed-css 0';
+		$cmd .= ' --embed-font 0';
+		$cmd .= ' --bg-format "jpg"';
+		$cmd .= ' --dest-dir "'.$view_path.'"';
+		// $cmd .= ' --page-filename "'.$doc_url.'-%03d.page"';
+		// $cmd .= ' --page-filename "%03d"';
+		$cmd .= ' --page-filename "%03d.html"';
+		// $cmd .= ' --css-filename "'.$doc_url.'.css"';
+		$cmd .= ' --css-filename "page.min.css"';
+		$cmd .= ' --embed-javascript 0';
+		$cmd .= ' --process-outline 0';
+		$cmd .= ' --vdpi 80';
+		$cmd .= ' --hdpi 80';
+		$cmd .= ' "'.self::$convert_path.$doc_url.'.pdf"';
+		exec($cmd, $r);
+		if( $file['extension'] != 'pdf' ){
+			unlink(self::$convert_path.$doc_url.".pdf");
+		}
+		// $this->clearn_file($view_path, 'woff');
+		// $file_content = file_get_contents($view_path.'\page.min.css');
+		// preg_match_all('/@font-face{font-family:(.*?)}/i', $file_content, $imgArr);
+		// foreach ($imgArr[0] as $key => $value) {
+		// 	$file_content = str_replace($value, '', $file_content);
+		// }
+		// preg_match_all('/.ff[0-9]*{(.*?)}/i', $file_content, $imgArr);
+		// foreach ($imgArr[1] as $key => $value) {
+		// 	$file_content = str_replace($value, '', $file_content);
+		// }
+		// file_put_contents($view_path.'\page.min.css', $file_content);
+		if($update_doc_content){
+			$doc_content = "";
+		}
+		for($i=1;$i<=$page_num;$i++){
+			$file_content = file_get_contents($view_path.'\\'.sprintf("%03d.html", $i));
+			//
+			// preg_match_all('/<img.+src=\"?(.+\.(jpg|gif|bmp|bnp|png))\"?.+>/i', $file_content, $imgArr);
+			// $file_content = str_replace($imgArr[1][0], 'http://view.mmdili.com/'.$user_url.'/'.$doc_url.'/'.$imgArr[1][0], $file_content);
+			//
+			// preg_match_all('/<\s*img\s+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i', $file_content, $imgArr); 
+				preg_match_all('/<\s*img\s+[^>]*?class\s*=\s*(\'|\")(.*?)\\1+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i', $file_content, $imgArr); 
+				if( isset($imgArr[2][0]) && isset($imgArr[4][0]) ){
+					$div = '<div class="'.$imgArr[2][0].'" style="background-image:url('.'http://view.mmdili.com/'.$user_url.'/'.$doc_url.'/'.$imgArr[4][0].')"></div>';
+					$file_content = str_replace($imgArr[0][0], $div, $file_content);
+				}
+			//
+			preg_match_all('/<a .*?href="(.*?)".*?>/is', $file_content, $imgArr);
+			foreach($imgArr[1] as $key => $value){
+				$file_content = str_replace($value, 'javascript:;', $file_content);
+			}
+			file_put_contents($view_path.'\\'.sprintf("%03d.html", $i), $file_content);
+			if($update_doc_content){
+				if($doc_content == ""){
+					$doc_content = $file_content;
+				}else{
+					$doc_content .= '#[page]#'.$file_content;
+				}
+		
+			}
+		}
+		$this->clearn_file($view_path, 'html');
+		$views = $this->oss->listView($user_url, $doc_url);
+		$objects = array();
+		foreach ($views as $key => $value) {
+			$objects[] = $value->getKey();
+		}
+		if( count($objects) && !$this->oss->deleteObjects($objects)){
+			echo "delete html from OSS failed.";
+			return;
+		}
+		if( !$this->oss->uploadDir($user_url.'/'.$doc_url, $view_path)){
+			echo "upload html to OSS failed.";
+			return;
+		}
+		$this->file->del_dir_file(self::$view_path);
+		//
+		if( $update_doc_content ){
+			//$doc_content = iconv('GB2312', 'UTF-8//IGNORE', $doc_content);
+			$doc_content = $this->trim_whitespace($doc_content);
+		}
+		//
+		if( !$this->oss->uploadDir($this->user_url[$doc_user_id].'/'.$time, $view_optimizer_path)){
+			echo "upload swf to OSS failed.";
+			return;
+		}
+		$this->file->del_dir_file(self::$view_path);
+		
+		if( !is_dir($online_path) ){
+			mkdir($online_path, 0777, true);
+		}
+		
+		if( !$this->oss->uploadFile(iconv('GB2312', 'UTF-8', $this->user_url[$doc_user_id].'/'.strtotime(date('Y', $time).'-01-01 00:00:00').'/'.$file['basename']), iconv('GB2312', 'UTF-8', self::$convert_path.$file['basename'])) ){					
+			echo "upload doc to OSS failed.";
+			return;
+		}
+		rename(self::$convert_path.$file['basename'], $online_path.$file['basename']);
+
+		$doc_content = iconv('GB2312', 'UTF-8//IGNORE', $doc_content);
+		$doc_content = $this->trim_whitespace($doc_content);
+		switch($file['extension']){
+			case 'doc':
+				$doc_ext = 1;
+				break;
+			case 'docx':
+				$doc_ext = 2;
+				break;
+			case 'pdf':
+				$doc_ext = 3;
+				break;
+			case 'ppt':
+				$doc_ext = 4;
+				break;
+			case 'pptx':
+				$doc_ext = 5;
+				break;
+			case 'txt':
+				$doc_ext = 6;
+				break;
+			case 'xls':
+				$doc_ext = 7;
+				break;
+			case 'xlsx':
+				$doc_ext = 8;
+				break;
+			default:
+				$doc_ext = 0;
+				break;
+		}
+		$this->upload_model->insert_doc($time, iconv('GB2312', 'UTF-8', $file['filename']), $doc_content, $doc_user_id, $doc_ext, $doc_cate_id, $page_width, $page_height, $page_num, intval(!empty($poly2bitmap)), $doc_dl_forbidden);
+		header('Location:'.base_url('upload'));
+	}
 }
