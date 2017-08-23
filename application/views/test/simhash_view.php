cd14ceba4e27187aaedc1bfc02072e58b05faec0
miaojuanfeng <miaojuanfeng@qq.com>
2017-08-23 12:20:41 +0800

    asdasd

--- application/views/test/simhash_view.php
+++ application/views/test/simhash_view.php
@@ -4,9 +4,37 @@
 	<title>Simhash PHP Extension Demo</title>
 </head>
 <style>
-
+.left{
+	float: left;
+}
+.right{
+	float: right;
+}
+.clearfix{
+	float: none;
+	clear: both;
+}
+.text1{
+	margin:10px;
+}
+.text2{
+	margin:10px;
+}
 </style>
 <body>
-	
+	<div class="container">
+		<div class="left">
+			<div class="text1">
+				<textarea>aasdasdas</textarea>
+			</div>
+		</div>
+		<div class="right">
+			<div class="text2">
+				<textarea>aasdasdas</textarea>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="result"></div>
+	</div>
 </body>
 </html>
